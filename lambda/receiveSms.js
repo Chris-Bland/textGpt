36dac7cc79c2529ea21162e90f2f5598e01923d2
Chris <ssd@Chriss-MacBook-Pro.local>
2023-06-14 13:07:30 -0400

    updating receive to call query

--- lambda/receiveSms.js
+++ lambda/receiveSms.js
@@ -1,8 +1,20 @@
-exports.handler = async function(event) {
-    console.log(`ReceiveSMS Lambda -- request: ${JSON.stringify(event, undefined, 2)}`);
-    return {
-      statusCode: 200,
-      headers: { "Content-Type": "text/plain" },
-      body: `You've hit receive: ${event.path}\n`
-    };
+const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda();
+
+exports.handler = async (event) => {
+console.log(`ReceiveSMS Lambda -- request: ${JSON.stringify(event, undefined, 2)}`);
+  const params = {
+    FunctionName: 'QueryGptHandler',
+    InvocationType: 'RequestResponse',
+    Payload: JSON.stringify(event),
   };
+
+  try {
+    const response = await lambda.invoke(params).promise();
+    const responseData = JSON.parse(response.Payload);
+    console.log(`Response Data from Query: ${responseData}`);
+  } catch (err) {
+    console.error(`Receive Failed because: ${err}`);
+    throw err;
+  }
+};
--- lib/text_gpt-stack.ts
+++ lib/text_gpt-stack.ts
@@ -1,10 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class TextGptStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //Lambdas:
     const receiveSms = new lambda.Function(this, 'ReceiveSmsHandler', {
       runtime: lambda.Runtime.NODEJS_16_X,
       code: lambda.Code.fromAsset('lambda'),
@@ -22,6 +25,19 @@ export class TextGptStack extends cdk.Stack {
       code: lambda.Code.fromAsset('lambda'),
       handler: 'queryGpt.handler'
     });   
+
+
+  // Grant receiveSms invoke permissions for queryGpt
+    queryGpt.grantInvoke(receiveSms);
+    receiveSms.addToRolePolicy(new iam.PolicyStatement({
+      actions: ['lambda:InvokeFunction'],
+      resources: [queryGpt.functionArn] 
+    }));
+
+    //API Gateway for receive. send/query do not need
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: receiveSms
+    });
 
   }
 }
