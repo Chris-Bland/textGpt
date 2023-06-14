ee4456e9a1e355851030600d9c6d38be4b03a33f
Chris <ssd@Chriss-MacBook-Pro.local>
2023-06-14 12:32:50 -0400

    Creating lambdas

--- lib/text_gpt-stack.ts
+++ lib/text_gpt-stack.ts
@@ -1,8 +1,27 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
-export class TextGptStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class TextGptStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const receiveSms = new lambda.Function(this, 'ReceiveSmsHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'receiveSms.handler'
+    });
+
+    const sendSms = new lambda.Function(this, 'SendSmsHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'sendSms.handler'
+    });
+
+    const queryGpt = new lambda.Function(this, 'QueryGptHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'queryGpt.handler'
+    });   
+
   }
 }
