6113dade6a56125bdf5d89da95006f10dbdea7f0
Chris <ssd@Chriss-MacBook-Pro.local>
2023-06-20 12:12:13 -0400

    reorganization of the stack

---
+++ lib/config.ts
@@ -0,0 +1,23 @@
+export const envConfig = {
+    receiveSms: {
+        memorySize: 256,
+        timeout: 10, // in seconds
+        entry: './lambda/receiveSms.js'
+    },
+    queryGpt: {
+        memorySize: 1024,
+        timeout: 30, // in seconds
+        entry: './lambda/queryGpt.js',
+        bundling: {
+            nodeModules: ['openai']
+        }
+    },
+    sendSms: {
+        memorySize: 256,
+        timeout: 10, // in seconds
+        entry: './lambda/sendSms.js',
+        bundling: {
+            nodeModules: ['twilio']
+        }
+    }
+}
--- lib/text_gpt-stack.ts
+++ lib/text_gpt-stack.ts
@@ -1,5 +1,4 @@
 import * as cdk from 'aws-cdk-lib';
-// import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -7,89 +6,74 @@ import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import { envConfig } from './config';
 
+type CustomNodejsFunctionOptions = {
+  memorySize: number;
+  timeout: number;
+  entry: string;
+  bundling?: {
+      nodeModules: string[];
+  };
+};
 
 export class TextGptStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-    
-//Lambdas:
-    const receiveSms = new NodejsFunction(this, 'ReceiveSmsHandler', {
-      runtime: lambda.Runtime.NODEJS_16_X,
-      architecture: lambda.Architecture.ARM_64, //remove if not on M series Mac
-      memorySize: 256,
-      timeout: cdk.Duration.seconds(10),  
-      entry: './lambda/receiveSms.js', 
-      handler: 'handler',
-      // bundling: {
-      //   nodeModules: ['uuid'], 
-      // },
-  });
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    //increase memory and timeout for openai request
-    const queryGpt = new NodejsFunction(this, 'QueryGptHandler', {
-      runtime: lambda.Runtime.NODEJS_16_X,
-      architecture: lambda.Architecture.ARM_64, //remove if not on M series Mac
-      memorySize: 256,
-      timeout: cdk.Duration.seconds(30),  
-      entry: './lambda/queryGpt.js', 
-      handler: 'handler',
-      bundling: {
-        nodeModules: ['openai'], 
-      },
-    });
+        // Lambdas
+        const receiveSms = this.createLambdaFunction('ReceiveSmsHandler', envConfig.receiveSms);
+        const queryGpt = this.createLambdaFunction('QueryGptHandler', envConfig.queryGpt);
+        const sendSms = this.createLambdaFunction('SendSmsHandler', envConfig.sendSms);
 
-    //increase memory and timeout for Twilio create
-    const sendSms = new NodejsFunction(this, 'SendSmsHandler', {
-      runtime: lambda.Runtime.NODEJS_16_X,
-      architecture: lambda.Architecture.ARM_64, //remove if not on M series Mac
-      memorySize: 256,
-      timeout: cdk.Duration.seconds(10),  
-      entry: './lambda/sendSms.js', 
-      handler: 'handler',
-      bundling: {
-        nodeModules: ['twilio'], 
-      },
+        // Secrets
+        const secret = secretsmanager.Secret.fromSecretNameV2(this, 'ImportedSecret', 'ChatGPTSecrets');
+        sendSms.addToRolePolicy(new iam.PolicyStatement({
+            actions: ['secretsmanager:GetSecretValue'],
+            resources: [secret.secretArn],
+        }));
+        queryGpt.addToRolePolicy(new iam.PolicyStatement({
+            actions: ['secretsmanager:GetSecretValue'],
+            resources: [secret.secretArn],
+        }));
 
-    });
+        // SQS
+        const receiveSmsQueue = new sqs.Queue(this, 'ReceiveSmsQueue', {
+            visibilityTimeout: cdk.Duration.seconds(30)
+        });
+        const sendSmsQueue = new sqs.Queue(this, 'SendSmsQueue', {
+            visibilityTimeout: cdk.Duration.seconds(30)
+        });
 
+        // Environment Variables
+        receiveSms.addEnvironment('SMS_QUEUE_URL', receiveSmsQueue.queueUrl);
+        queryGpt.addEnvironment('SMS_QUEUE_URL', receiveSmsQueue.queueUrl);
+        queryGpt.addEnvironment('SEND_SMS_QUEUE_URL', sendSmsQueue.queueUrl);
+        sendSms.addEnvironment('SEND_SMS_QUEUE_URL', sendSmsQueue.queueUrl);
 
-//Secrets:
-  const secret = secretsmanager.Secret.fromSecretNameV2(this, 'ImportedSecret', 'ChatGPTSecrets');
-  sendSms.addToRolePolicy(new iam.PolicyStatement({
-    actions: ['secretsmanager:GetSecretValue'],
-    resources: [secret.secretArn],
-  }));
-  queryGpt.addToRolePolicy(new iam.PolicyStatement({
-    actions: ['secretsmanager:GetSecretValue'],
-    resources: [secret.secretArn],
-  }));
+        // Permissions
+        receiveSmsQueue.grantSendMessages(receiveSms);
+        sendSmsQueue.grantSendMessages(queryGpt);
 
-//SQS
-    const receiveSmsQueue = new sqs.Queue(this, 'ReceiveSmsQueue', {
-      visibilityTimeout: cdk.Duration.seconds(30) // Adjust as needed
-    });
-    const sendSmsQueue = new sqs.Queue(this, 'SendSmsQueue', {
-      visibilityTimeout: cdk.Duration.seconds(30) // Adjust as needed
-    });
+        // Event Sources
+        queryGpt.addEventSource(new lambdaEventSources.SqsEventSource(receiveSmsQueue));
+        sendSms.addEventSource(new lambdaEventSources.SqsEventSource(sendSmsQueue));
 
-// Set up the environment variables for the receiveSms and queryGpt functions
-    receiveSms.addEnvironment('SMS_QUEUE_URL', receiveSmsQueue.queueUrl);
-    queryGpt.addEnvironment('SMS_QUEUE_URL', receiveSmsQueue.queueUrl);
-    queryGpt.addEnvironment('SEND_SMS_QUEUE_URL', sendSmsQueue.queueUrl);
-    sendSms.addEnvironment('SEND_SMS_QUEUE_URL', sendSmsQueue.queueUrl);
+        // API Gateway
+        new apigw.LambdaRestApi(this, 'Endpoint', {
+            handler: receiveSms
+        });
+    }
 
-// Allow the receiveSms and queryGpt functions to send messages to their respective queues
-    receiveSmsQueue.grantSendMessages(receiveSms);
-    sendSmsQueue.grantSendMessages(queryGpt);
-
-// Set up the event source mappings for the queryGpt and sendSms functions
-    queryGpt.addEventSource(new lambdaEventSources.SqsEventSource(receiveSmsQueue));
-    sendSms.addEventSource(new lambdaEventSources.SqsEventSource(sendSmsQueue));
-
-//API Gateway for receive. send/query do not need
-    new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: receiveSms
-    });
+    private createLambdaFunction(id: string, options: CustomNodejsFunctionOptions) {
+      return new NodejsFunction(this, id, {
+          ...options,
+          timeout: cdk.Duration.seconds(options.timeout),
+          runtime: lambda.Runtime.NODEJS_16_X,
+          architecture: lambda.Architecture.ARM_64, //remove if not on M series Mac
+          //architecture: lambda.Architecture.X86_64,
+          handler: 'handler',
+      });
   }
+  
 }
