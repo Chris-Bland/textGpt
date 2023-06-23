e47ace9cf3fd5cfc702f06b297b0cd3294ab48dd
Chris <ssd@Chriss-MacBook-Pro.local>
2023-06-23 16:59:51 -0400

    resolving double message on dynamodb error

--- lambda/queryGpt.ts
+++ lambda/queryGpt.ts
@@ -6,13 +6,12 @@ import { DynamoDB } from 'aws-sdk';
 const dynamodb = new DynamoDB.DocumentClient();
 
 export const handler = async (event: any): Promise<any> => {
+  try{
   const secrets = await getSecret('ChatGPTSecrets');
   console.log(`GPT -- ${JSON.stringify(event.Records)}`)
   
   if (!secrets || !secrets.OPENAI_API_KEY) {
     console.error('QueryGPT -- Unable to retrieve OpenAI API Key from secrets');
-    const { conversationId, to, from, body } = JSON.parse(event.Records[0].record.body);
-    return;
   }
 
   const configuration = new Configuration({
@@ -21,8 +20,8 @@ export const handler = async (event: any): Promise<any> => {
   const openai = new OpenAIApi(configuration);
 
   for (const record of event.Records) {
-    const { conversationId, to, from, body } = JSON.parse(record.body);
-    try {
+      const { conversationId, to, from, body } = JSON.parse(record.body);
+      //Dynamo Db get info:
       console.log(`${conversationId} -- QueryGPT -- Building Prompt and Calling OpenAI`);
       const holden = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
@@ -38,31 +37,36 @@ export const handler = async (event: any): Promise<any> => {
       const openAIResponse = holden.data.choices && holden.data.choices[0].message ? holden.data.choices[0].message.content : undefined;
 
       if (openAIResponse) {      
-        
         const message = {conversationId: conversationId, to: to, from: from, body: openAIResponse};
         await sendMessageToSqs(message, 'QueryGPT', process.env.SEND_SMS_QUEUE_URL);
-        
-        const params = {
-          TableName: 'ConversationTable', 
-          Item: {
-            senderNumber:to,
-            TwilioNumber:from,
-            input: body,
-            response: openAIResponse,
-            conversationId: conversationId,
-            timestamp: new Date().toISOString(),
-          },
-         };
-          await dynamodb.put(params).promise();
-          console.log(`Stored history in DynamoDB for conversationId: ${conversationId}`);
-
+      
+      //dynamoDb stuff:
+      try{
+      const params = {
+        TableName: 'ConversationTable', 
+        Item: {
+          senderNumber:to,
+          TwilioNumber:from,
+          input: body,
+          response: openAIResponse,
+          conversationId: conversationId,
+          timestamp: new Date().toISOString(),
+        },
+       };
+        await dynamodb.put(params).promise();
+        console.log(`Stored history in DynamoDB for conversationId: ${conversationId}`);
+      }catch(error){
+        console.error(`${conversationId} -- QueryGPT -- Failure to store dynamoDb entry for conversation: ${JSON.stringify(error)}`)
+        return
+      }
       } else {
         console.error(`${conversationId} -- QueryGPT -- No response from OpenAI`);
       }
-    } catch (error) {
-      console.error(`${conversationId} -- QueryGPT -- Error: ${JSON.stringify(error)}`);
-      const errorMessage = {conversationId: conversationId, to: to, from: from, body: JSON.stringify(error)};
-      await sendMessageToSqs( errorMessage, 'QueryGPT', process.env.ERROR_QUEUE_URL );
-    }
+      
+  }
+  } catch (error) {
+    console.error(`${event.Records[0].body.conversationId} -- QueryGPT -- Error: ${JSON.stringify(error)}`);
+    const errorMessage = {conversationId: event.Records[0].body.conversationId, to: event.Records[0].body.to, from: event.Records[0].body.from, body: JSON.stringify(error)};
+    await sendMessageToSqs( errorMessage, 'QueryGPT', process.env.ERROR_QUEUE_URL );
   }
 };
