bee58d589f46cdd982a8bd4c02ebba2b525527c5
Chris-Bland <kitecabrinha@gmail.com>
2023-06-28 12:46:08 -0400

    finished secrets utils test case, woot

--- lambda/test/utils/secrets.test.ts
+++ lambda/test/utils/secrets.test.ts
@@ -1,67 +1,64 @@
-import { SecretsManager } from 'aws-sdk';
-import { getSecret } from '../../utils/secrets.util'; 
+import { getSecret } from '../../utils/secrets.util';
 
-jest.mock('aws-sdk', () => {
-  const mockGetSecretValue = jest.fn();
-  return {
-    SecretsManager: jest.fn(() => {
-      return { getSecretValue: mockGetSecretValue };
-    }),
+const baseResponse = {
+    ARN: 'x',
+    Name: 'test_creds',
+    VersionId: 'x',
+    VersionStages: ['x'],
+    CreatedDate: 'x'
   };
-});
+  
+  jest.mock('aws-sdk', () => {
+    return {
+      config: {
+        update(val: any) {},
+      },
+      SecretsManager: function () {
+        return {
+          getSecretValue: jest.fn(({ SecretId }: { SecretId: string }) => {
+            if (SecretId === 'testSid') {
+              return {
+                promise: function () {
+                  return { ...baseResponse, SecretString: '{"testSid":"test123"}' };
+                },
+              };
+            } else if (SecretId === 'errorCase') {
+              throw new Error('Error calling SecretsManager');
+            } else {
+              return {
+                promise: function () {
+                  return { ...baseResponse };
+                },
+              };
+            }
+          }),
+        };
+      },
+    };
+  });
+  
 
 describe('getSecret', () => {
-  let secretsManager: {
-    getSecretValue: jest.Mock;
-  };
-
   beforeEach(() => {
-    secretsManager = {
-      getSecretValue: jest.fn(),
-    };
-  
-    secretsManager.getSecretValue.mockClear();
     console.error = jest.fn();
   });
 
   it('should retrieve secret successfully with SecretString', async () => {
-    secretsManager.getSecretValue.mockResolvedValue({
-      SecretString: JSON.stringify({ key: 'value' }),
-    });
-
-    const secret = await getSecret('secretName');
-    expect(secret).toEqual({ key: 'value' });
+    const secret = await getSecret('testSid');
+    expect(secret).toEqual({ testSid: 'test123' });
   });
 
-  it('should retrieve secret successfully with SecretBinary', async () => {
-    const buffer = Buffer.from('binarySecret', 'ascii');
-    secretsManager.getSecretValue.mockResolvedValue({
-      SecretBinary: buffer.buffer as ArrayBuffer,
-    });
-
-    const secret = await getSecret('secretName');
-    expect(secret).toEqual('binarySecret');
-  });
-
-  it('should handle no SecretString or SecretBinary in the secret', async () => {
-    secretsManager.getSecretValue.mockResolvedValue({
-      promise: jest.fn().mockResolvedValue({testData: 'test'}),
-    });
-  
-    const secret = await getSecret('secretName');
-    expect(console.error).toHaveBeenCalledWith(
-      'No SecretString or SecretBinary found in the secret'
-    );
+  it('should handle no SecretString in the secret', async () => {
+    const secret = await getSecret('noSecretString');
+    expect(console.error).toHaveBeenCalledWith('No SecretString found in the secret');
     expect(secret).toBe(null);
   });
-  
 
   it('should handle error in retrieving secret', async () => {
-    secretsManager.getSecretValue.mockRejectedValue({
-        promise: jest.fn().mockRejectedValue(new Error('Error retrieving secret')),
-      });
-    const secret = await getSecret('secretName');
-    expect(console.error).toHaveBeenCalledWith("Error retrieving secret: TypeError: Cannot read properties of undefined (reading 'promise')");
+    const secret = await getSecret('errorCase');
+    expect(console.error).toHaveBeenCalledWith(
+      'Error retrieving secret: Error: Error calling SecretsManager'
+    );
     expect(secret).toBe(null);
   });
 });
--- lambda/utils/secrets.util.ts
+++ lambda/utils/secrets.util.ts
@@ -5,14 +5,12 @@ const secretsManager = new SecretsManager()
 export async function getSecret (secretName: string): Promise<any> {
   try {
     const data = await secretsManager.getSecretValue({ SecretId: secretName }).promise()
-    console.log(`data: ${data}`);
+    console.log(`secretName: ${secretName}. data: ${JSON.stringify(data)}`);
     if ('SecretString' in data && data.SecretString) {
       return JSON.parse(data.SecretString)
-    } else if (data.SecretBinary) {
-      const buff = Buffer.from(data.SecretBinary as ArrayBuffer)
-      return buff.toString('ascii')
     } else {
-      console.error('No SecretString or SecretBinary found in the secret')
+      console.error('No SecretString found in the secret')
+      return null;
     }
   } catch (error) {
     console.error(`Error retrieving secret: ${error}`)
