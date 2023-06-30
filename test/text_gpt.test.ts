cfca2dffa722c20983031184b72b560dc1f7654d
Chris-Bland <kitecabrinha@gmail.com>
2023-06-30 15:27:58 -0400

    updating stack testcases

--- test/text_gpt.test.ts
+++ test/text_gpt.test.ts
@@ -1,47 +1,55 @@
-import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
+import { expect as expectCDK, haveResource, haveResourceLike } from '@aws-cdk/assert';
 import { App } from 'aws-cdk-lib';
 import { TextGptStack } from '../lib/text_gpt-stack';
 
 test('Test TextGptStack has three SQS Queues with specific names', () => {
   const app = new App();
   const stack = new TextGptStack(app, 'TestTextGptStack');
+  const template = app.synth().getStackArtifact(stack.artifactId).template;
+  const expectedQueueNames = [
+    /ReceiveSmsQueue/,
+    /SendSmsQueue/,
+    /ErrorSmsQueue/,
+  ];
 
-  // Assert that the specific SQS queues are present
-  expectCDK(stack).to(haveResource('AWS::SQS::Queue', {
-    QueueName: 'ReceiveSmsQueue',
-  }));
-  expectCDK(stack).to(haveResource('AWS::SQS::Queue', {
-    QueueName: 'SendSmsQueue',
-  }));
-  expectCDK(stack).to(haveResource('AWS::SQS::Queue', {
-    QueueName: 'ErrorSmsQueue',
-  }));
+  for (const queueNameRegex of expectedQueueNames) {
+    const queueName = Object.keys(template.Resources).find((resourceName) =>
+      queueNameRegex.test(resourceName)
+    );
+    expect(queueName).toBeDefined();
+  }
 });
+
+
 
 test('Test TextGptStack has Lambda Functions with specific names', () => {
   const app = new App();
   const stack = new TextGptStack(app, 'TestTextGptStack');
+  const template = app.synth().getStackArtifact(stack.artifactId).template;
+  const expectedLambdaNames = [
+    /ReceiveSmsHandler/,
+    /QueryGptHandler/,
+    /SendSmsHandler/,
+  ];
 
-  // Assert that the specific Lambda functions are present
-  expectCDK(stack).to(haveResource('AWS::Lambda::Function', {
-    FunctionName: 'ReceiveSmsHandler',
-  }));
-  expectCDK(stack).to(haveResource('AWS::Lambda::Function', {
-    FunctionName: 'QueryGptHandler',
-  }));
-  expectCDK(stack).to(haveResource('AWS::Lambda::Function', {
-    FunctionName: 'SendSmsHandler',
-  }));
+  for (const lambdaNameRegex of expectedLambdaNames) {
+    const lambdaName = Object.keys(template.Resources).find((resourceName) =>
+      lambdaNameRegex.test(resourceName)
+    );
+    expect(lambdaName).toBeDefined();
+  }
+
 });
 
 test('Test TextGptStack has a DynamoDB Table with a specific name', () => {
   const app = new App();
   const stack = new TextGptStack(app, 'TestTextGptStack');
-
-  // Assert that the specific DynamoDB table is present
-  expectCDK(stack).to(haveResource('AWS::DynamoDB::Table', {
-    TableName: 'ConversationTable',
-  }));
+  const template = app.synth().getStackArtifact(stack.artifactId).template;
+  const expectedTableNames = /ConversationTable/;
+  const tableName = Object.keys(template.Resources).find((resourceName) =>
+  expectedTableNames.test(resourceName)
+);
+expect(tableName).toBeDefined();
 });
 
 test('Test TextGptStack has an API Gateway', () => {
